e698c688340511d6eb3cc05b8111eb38d268ae6b
Leonardo Aristizabal <leonardo.aristizabal.m@gmail.com>
2023-03-10 20:00:35 -0400

    add bot.js, package.json and .gitignore

---
+++ bot.js
@@ -0,0 +1,28 @@
+const { Telegraf }= require('telegraf');
+
+const bot = new Telegraf('6102990420:AAH9hKcIhf8Z4WXUJMMjQgCBzekltljdJHI');
+
+bot.start((ctx) => { //comando start
+    console.log(ctx)
+    ctx.reply('Hola, como te puedo ayudar hoy?'); //respuesta
+})
+bot.help((ctx) => {
+    ctx.reply('Ayuda');
+})
+bot.settings((ctx) => {
+    ctx.reply('Configuraciones');
+})
+
+bot.command(['mycommand', 'Mycommando', 'MYCOMMAND', 'test'], (ctx)=> { //diferentes entradas de texto
+    ctx.reply('my custom command')
+})
+
+bot.hears('computer', ctx => {  //Cuando se escribe y manda la palabra computer
+    ctx.reply('Hey, escuche que dijiste computador(?)') 
+})
+
+bot.on('text', ctx => {
+    ctx.reply('estas texteando')
+}) //cuando el escuche un evento, cualquier cosa (sickers, audio, iconos...)
+
+bot.launch();
