edc8861c6dac899ba79d89960a86423c4829e3c5
Leonardo Aristizabal <leonardo.aristizabal.m@gmail.com>
2023-03-11 16:57:23 -0400

    modificacion en respuestas a mensajes tipo text

--- bot.js
+++ bot.js
@@ -4,7 +4,10 @@ const bot = new Telegraf('6102990420:AAH9hKcIhf8Z4WXUJMMjQgCBzekltljdJHI');
 
 bot.start((ctx) => { //comando start
     console.log(ctx)
-    ctx.reply('Hola, como te puedo ayudar hoy?'); //respuesta
+    console.log(ctx.chat)
+    console.log(ctx.message)
+    console.log(ctx.updateSubTypes)
+    ctx.reply(`Hola ${ctx.from.first_name}, como te puedo ayudar hoy?`); //respuesta
 })
 bot.help((ctx) => {
     ctx.reply('Ayuda');
@@ -17,12 +20,18 @@ bot.command(['mycommand', 'Mycommando', 'MYCOMMAND', 'test'], (ctx)=> { //difere
     ctx.reply('my custom command')
 })
 
+bot.hears('Hola', ctx => {  //Cuando se escribe y manda la palabra computer
+    ctx.reply('Hola! Recuerda que debes escribir con los comandos del tipo: /Start') 
+})
+
 bot.hears('computer', ctx => {  //Cuando se escribe y manda la palabra computer
     ctx.reply('Hey, escuche que dijiste computador(?)') 
 })
 
 bot.on('text', ctx => {
-    ctx.reply('estas texteando')
-}) //cuando el escuche un evento, cualquier cosa (sickers, audio, iconos...)
+ ctx.reply('Recuerda que debes escribir con los comandos del tipo: /Start'
+ )}) //cuando el escuche un evento, cualquier cosa (sickers, audio, iconos...)
+
+
 
 bot.launch();
